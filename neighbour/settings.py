e1998bd34f5ca920f2a2a9ed1ddac14a223808f6
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-23 12:09:33 +0300

    ft:deployment to heroku

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-MODE='dev'
+DEBUG = False
+MODE='prod'
 
 ALLOWED_HOSTS = ['.localhost', '.herokuapp.com', '127.0.0.1']
 

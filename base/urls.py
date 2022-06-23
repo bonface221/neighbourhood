8aae5a7de36d6a7695ac495ff38d9da1b32d6b70
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-23 10:53:42 +0300

    ft:added the project's urls

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[ 
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('login/',views.LoginInterfaceView.as_view(),name='login'),
+    path('register/',views.SignupView.as_view(),name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+
 ]

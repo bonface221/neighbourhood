ed153647729643378ad9ddbced3f9ecf7ead6859
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-23 10:52:26 +0300

    ft:added the admin config

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Profile, Businesses,Message,Neighborhoods_cool
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Businesses)
+admin.site.register(Message)
+admin.site.register(Neighborhoods_cool)

b5cffe7ca20ee4dcfb73360eb96f0bda1342b5fa
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-23 10:55:59 +0300

    ft:added the projects forms

---
+++ base/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Neighborhoods_cool
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
+
+class NeighbourhoodForm(forms.ModelForm):
+
+	class  Meta:
+		model = Neighborhoods_cool
+		fields=['name','picture', 'location','description','health_contact','police_contact']
+	
